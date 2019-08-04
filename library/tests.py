27b5b17972df8dd5d75b108c2de29ea47f4be393
BuildTools <unconfigured@null.spigotmc.org>
2019-08-05 01:05:44 +0200

    Coverage tests

--- library/tests.py
+++ library/tests.py
@@ -19,9 +19,12 @@ class Tester:
         voice_channel = self.shiro.get_channel(self.shiro.config["tests"]["voice_channel"])
         message = await text_channel.send("Starting tests!")
         message.author.voice.channel = voice_channel
+        prefix = await self.shiro.get_prefix(message)
+        message.content = "{0}info".format(prefix)
         self.ctx = self.shiro.get_context(message)
 
     async def test_commands(self):
         """Run tests for every command"""
         for command in self.shiro.walk_commands():
-            pass
+            self.ctx.command = command
+            await self.shiro.invoke(self.ctx)

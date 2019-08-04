bf2d5b3d56317ba277175a0e40c97f68d14a229d
BuildTools <unconfigured@null.spigotmc.org>
2019-08-05 01:42:52 +0200

    Coverage tests

--- library/tests.py
+++ library/tests.py
@@ -1,3 +1,5 @@
+import logging
+
 class Tester:
     def __init__(self, shiro):
         self.shiro = shiro
@@ -7,13 +9,15 @@ class Tester:
         """Start tests"""
         await self.create_ctx()
         await self.test_commands()
-        self.shiro.shutdown()
 
     async def create_ctx(self):
         """Create fake ctx to invoke commands under"""
         text_channel = self.shiro.get_channel(self.shiro.config["tests"]["text_channel"])
+        logging.info(text_channel)
         voice_channel = self.shiro.get_channel(self.shiro.config["tests"]["voice_channel"])
+        logging.info(voice_channel)
         message = await text_channel.send("Starting tests!")
+        logging.info(message)
         message.author.voice.channel = voice_channel
         prefix = await self.shiro.get_prefix(message)
         message.content = "{0}info".format(prefix)
